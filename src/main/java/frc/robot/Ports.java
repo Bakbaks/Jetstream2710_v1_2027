a2c36ec9ec814ab7177331aaa0298d8c0db4cdf3
Bakbaks <allen4win@gmail.com>
2026-08-17 23:13:57 -0700

    Verify can bus ports and clarify physical wiring. '

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,7 +3,7 @@ package frc.robot;
 import com.ctre.phoenix6.CANBus;
 
 public final class Ports {
-  public static final CANBus RIO_CAN_BUS = new CANBus("rio");
+  public static final CANBus RIO_CAN_BUS = CANBus.systemcore(1);
 
   private Ports() {}
 }
